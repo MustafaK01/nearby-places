360969d621e9373f6dd95a35940bd3d16686b5e4
MustafaK01 <mestefe119@gmail.com>
2023-06-13 22:38:48 +0300

    add discovery server and define some settings

--- location-query-service/src/main/java/com/mustafak01/locationqueryservice/LocationQueryServiceApplication.java
+++ location-query-service/src/main/java/com/mustafak01/locationqueryservice/LocationQueryServiceApplication.java
@@ -2,8 +2,10 @@ package com.mustafak01.locationqueryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LocationQueryServiceApplication {
 
 	public static void main(String[] args) {
