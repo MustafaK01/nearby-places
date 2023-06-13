c310cc8d3968c8c5d9c6b8340415c85cf548b7a5
MustafaK01 <mestefe119@gmail.com>
2023-06-13 22:14:43 +0300

    creating project

---
+++ location-query-service/src/main/java/com/mustafak01/locationqueryservice/LocationQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mustafak01.locationqueryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationQueryServiceApplication.class, args);
+	}
+
+}
---
+++ location-query-service/src/test/java/com/mustafak01/locationqueryservice/LocationQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafak01.locationqueryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
