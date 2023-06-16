f19ff0d9669dbce31a85a0565d5afe10867f2127
MustafaK01 <mestefe119@gmail.com>
2023-06-16 16:37:44 +0300

    update

--- location-service/src/main/java/com/mustafak01/locationservice/controller/LocationController.java
+++ location-service/src/main/java/com/mustafak01/locationservice/controller/LocationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/location-service")
 @RequiredArgsConstructor
+@CrossOrigin
 public class LocationController {
 
     private final LocationService locationService;
